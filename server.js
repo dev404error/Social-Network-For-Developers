a9d81667b96c593f8ad326ba31909c0110e29ac6
“gadarsh555” <gadarsh555@gmail.com>
2020-04-12 16:58:37 +0530

    database mongodb atlas connection added

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
+// config module is installed and used to crete these json files which can be accessed from anywhere in the project
 
 const app = express();
 
+//connecting to databse
+connectDB();
+
+//giivng port a value  if it is run on heroku then process.env.PORT automatically gets the port value else port 5000 is assigned to it in loalhost environment
 const PORT = process.env.PORT || 5000;
 
 app.get('/',(req,res)=>{
